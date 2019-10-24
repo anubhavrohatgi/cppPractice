d0aa1c7c8e118d1c8e9eb79cb0c2c96659171cb4
Anubhav Rohatgi <anubhav.rohatgi@vnl.in>
2019-10-24 17:14:05 +0530

    added raiiThread example

--- cppThreads/raiiThread/main.cpp
+++ cppThreads/raiiThread/main.cpp
@@ -13,9 +13,10 @@ public:
 
     ~threadRAII() {
         if(th.joinable()) {
-            std::cout<<"\n[THREAD_RAII] Automatic JOIN"<<std::endl;
             th.join();
+            std::cout<<"\n[THREAD_RAII] Destroyed Object & Automatic JOIN"<<std::endl;
         }
+        std::cout<<"\n[THREAD_RAII] Destruction accomplished"<<std::endl;
     }
 
     //delete the copy constructor as thread cannot be copied or assigned
@@ -27,28 +28,57 @@ public:
 };
 
 
-class work {
+class work1 {
 public:
-    work(int x_) : x(x_) {
-
+    work1(int x_) : x(x_) {
+        ++count;
+        std::cout<<"\n[WORKER1] Ctor : "<<count<<std::endl;
     }
+
+    ~work1() {
+        std::cout<<"\n[WORKER1] Dtor : "<<count<<std::endl;
+    }
+
     void operator() () {
         for(int i =0; i < x; ++i) {
-            std::cout<<"[WORKER] Printing this :: "<<i<<std::endl;
+            std::cout<<"[WORKER1] Printing this :: "<<i<<std::endl;
         }
     }
 
 private:
     int x;
+    static int count;
 };
 
+int work1::count = 0;
+
+//class work2 {
+//public:
+//    work2(int x_) : x(x_) {
+
+//    }
+
+//    ~work2() {
+//        std::cout<<"\n[WORKER2] Destruction accomplished"<<std::endl;
+//    }
+
+//    void operator() () {
+//        for(int i =0; i < x; ++i) {
+//            std::cout<<"[WORKER2] Printing this :: "<<i<<std::endl;
+//        }
+//    }
+
+//private:
+//    int x;
+//};
 
 int main()
 {
     int local_main=5;
-    work my_func(local_main);
-    std::thread t(my_func);
-    threadRAII g(t);
+//    work1 my_func(local_main);
+    std::thread t((work1(local_main)));
+    t.join();
+//    threadRAII g(t);
     std::cout<<"\n[MAIN] From main thread..."<<std::endl;
     return 0;
 }
