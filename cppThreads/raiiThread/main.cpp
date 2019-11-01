8a575aa118d6035cdc116cd5c8e89288f6d397a5
Anubhav Rohatgi <anubhav.rohatgi@vnl.in>
2019-11-01 10:41:46 +0530

    raiiThread

--- cppThreads/raiiThread/main.cpp
+++ cppThreads/raiiThread/main.cpp
@@ -14,9 +14,9 @@ public:
     ~threadRAII() {
         if(th.joinable()) {
             th.join();
-            std::cout<<"\n[THREAD_RAII] Destroyed Object & Automatic JOIN"<<std::endl;
+            std::cout<<"\n"<<__PRETTY_FUNCTION__<<" Destroyed Object & Automatic JOIN"<<std::endl;
         }
-        std::cout<<"\n[THREAD_RAII] Destruction accomplished"<<std::endl;
+        std::cout<<"\n"<<__PRETTY_FUNCTION__<<" Destruction accomplished"<<std::endl;
     }
 
     //delete the copy constructor as thread cannot be copied or assigned
@@ -32,7 +32,7 @@ class work1 {
 public:
     work1(int x_) : x(x_) {
         ++count;
-        std::cout<<"\n[WORKER1] Ctor : "<<count<<std::endl;
+        std::cout<<"\n"<<__PRETTY_FUNCTION__<<" Ctor : "<<count<<std::endl;
     }
 
     work1(work1 const& other) : x(other.x)
@@ -48,12 +48,12 @@ public:
     }
 
     ~work1() {
-        std::cout<<"\n[WORKER1] Dtor : "<<count<<std::endl;
+        std::cout<<"\n"<<__PRETTY_FUNCTION__<<" Dtor : "<<count<<std::endl;
     }
 
     void operator() () {
         for(int i =0; i < x; ++i) {
-            std::cout<<"[WORKER1] Printing this :: "<<i<<std::endl;
+            std::cout<<__PRETTY_FUNCTION__<<" Printing this :: "<<i<<std::endl;
         }
     }
 
@@ -71,27 +71,41 @@ public:
     }
 
     ~work2() {
-        std::cout<<"\n[WORKER2] Dtor accomplished"<<std::endl;
+        std::cout<<"\n["<<__PRETTY_FUNCTION__<<"] Dtor accomplished"<<std::endl;
     }
 
     void operator() [[ noreturn ]] () {
-        throw "random";
+        throw "exception reported";
     }
 
 private:
     int x;
 };
 
-int main()
-{    
-    work1 w1{5};
-    work2 w2{5};
-    std::thread t1(std::ref(w1));
-    std::thread t2(std::ref(w2));
-//    t1.join();
-    threadRAII g1(t1);
-    threadRAII g2(t2);
+//int main()
+//{
+//    work1 w1{5};
+//    work2 w2{5};
+//    std::thread t1(std::ref(w1));
+////    std::thread t2(std::ref(w2));
+////    t1.join();
+//    threadRAII g1(t1);
+////    threadRAII g2(t2);
 
+//    std::cout<<"\n[MAIN] From main thread..."<<std::endl;
+//    return 0;
+//}
+
+
+void do_something() {
+    throw "exception reported";
+}
+
+
+int main(){
+    std::thread t(work1(5));
+    threadRAII g(t); // RAII
+    do_something(); // exceptions could be called!
     std::cout<<"\n[MAIN] From main thread..."<<std::endl;
     return 0;
 }
